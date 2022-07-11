f6989b6add304474fb8f81f5328dc81113ba5a75
Dilan <dilan@dilanxd.com>
2022-07-11 16:42:16 -0500

    update app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,53 +1,104 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 use discord_rpc_client::Client;
 use discord_rpc_client::models::rich_presence::Activity;
 use std::time::{SystemTime, UNIX_EPOCH};
-use tauri::State;
+use tauri::{State, CustomMenuItem, Menu, MenuItem, Submenu, AboutMetadata};
 use std::sync::Mutex;
 
 struct App {
-  client: Mutex<Client>,
+    client: Mutex<Client>,
 }
 
 fn main() {
-  tauri::Builder::default()
-    .manage(App {
-      client: Mutex::new(init_discord_client()),
-    })
-    .invoke_handler(tauri::generate_handler![update_presence])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .manage(App {
+            client: Mutex::new(init_discord_client()),
+        })
+        .invoke_handler(tauri::generate_handler![update_presence, clear_presence])
+        .menu(init_window_menu("SwitchPresence".to_string()))
+        .on_menu_event(|event| {
+            match event.menu_item_id() {
+                "sp-github" => {
+                    open::that("https://github.com/dilanx/switchpresence").unwrap();
+                }
+                "sp-clear" => {
+                    event.window().emit("event_clear_presence", {})
+                        .expect("failed to emit clear event");
+                }
+                _ => {}
+            }
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
 
 fn init_discord_client() -> Client {
-  let mut client = Client::new(995819278672601099);
-  client.start();
-  return client;
+    let mut client = Client::new(995819278672601099);
+    client.start();
+    return client;
 }
 
-#[tauri::command]
-fn update_presence(game: String, state: State<App>) {
-  let activity = Activity::new()
-    .state(&game)
-    .assets(|assets| {
-      assets
-        .large_image("switch")
-        .large_text(game)
-        .small_image("green")
-        .small_text("SwitchPresence")
-    })
-    .timestamps(|ts| {
-      let time = SystemTime::now();
-      let since_the_epoch = time
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards").as_secs();
-      ts.start(since_the_epoch)
-    });
+fn init_window_menu(title: String) -> Menu {
+    let mut menu = Menu::new();
+    if cfg!(target_os = "macos") {
+        let about = Submenu::new(
+            title,
+            Menu::new()
+                .add_native_item(
+                    MenuItem::About(
+                        "SwitchPresence".to_string(),
+                        AboutMetadata::new().copyright("Copyright © 2022 Dilan Nair (dilanxd.com)".to_string())
+                    )
+                )
+                .add_item(CustomMenuItem::new("sp-github", "View on GitHub"))
+                .add_native_item(MenuItem::Separator)
+                .add_native_item(MenuItem::Hide)
+                .add_native_item(MenuItem::HideOthers)
+                .add_native_item(MenuItem::ShowAll)
+                .add_native_item(MenuItem::Separator)
+                .add_native_item(MenuItem::Quit)
+        );
+        menu = menu.add_submenu(about);
+    }
+    let discord = Submenu::new(
+        "Discord",
+        Menu::new()
+            .add_item(CustomMenuItem::new("sp-clear", "Clear Rich Presence").accelerator("CmdOrCtrl+K"))
+    );
 
-  let mut client = state.client.lock().unwrap();
-  let _ = (*client).set_activity(|_| activity);
+    return menu.add_submenu(discord);
+    
+}
+
+#[tauri::command(async)]
+fn update_presence(game: String, state: State<App>) -> bool {
+    let activity = Activity::new()
+        .state(&game)
+        .assets(|assets| {
+            assets
+                .large_image("switch")
+                .large_text(game)
+                .small_image("green")
+                .small_text("SwitchPresence")
+        })
+        .timestamps(|ts| {
+            let time = SystemTime::now();
+            let since_the_epoch = time
+                .duration_since(UNIX_EPOCH)
+                .expect("Time went backwards").as_secs();
+            ts.start(since_the_epoch)
+        });
+    let mut client = state.client.lock().unwrap();
+    let act = (*client).set_activity(|_| activity);
+    return act.is_ok();
+}
+
+#[tauri::command(async)]
+fn clear_presence(state: State<App>) -> bool {
+    let mut client = state.client.lock().unwrap();
+    return (*client).clear_activity().is_ok();
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,148 +1,52 @@
+import { message } from '@tauri-apps/api/dialog';
 import { invoke } from '@tauri-apps/api/tauri';
-import { useState, useTransition } from 'react';
-import styled from 'styled-components';
+import { appWindow } from '@tauri-apps/api/window';
+import { useEffect, useState, useTransition } from 'react';
+import { RingLoader } from 'react-spinners';
 
 import games from './games.json';
+
+import './App.scss';
 
 interface Game {
   title: string;
   image?: string;
 }
 
-const ImageContainer = styled.div`
-  aspect-ratio: 16/9;
-  max-width: 240px;
-  display: block;
-  margin: 50px auto;
-  border-radius: 8px;
-  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
-  overflow: hidden;
-`;
-
-const Image = styled.img`
-  width: 100%;
-`;
-
-const NowPlaying = styled.p`
-  font-size: 16px;
-  margin: 10px 0;
-  text-align: center;
-  color: #737373;
-  font-weight: bold;
-`;
-
-const Title = styled.p`
-  font-size: 32px;
-  color: #f0f0f0;
-  font-weight: bold;
-  text-align: center;
-  margin: 0 40px 48px 40px;
-`;
-
-const Button = styled.button`
-  border: none;
-  display: block;
-  margin: 20px auto;
-  font-size: 16px;
-  font-weight: bold;
-  color: #a3a3a3;
-  background-color: #404040;
-  width: 240px;
-  height: 40px;
-  border-radius: 8px;
-
-  &:hover {
-    background-color: #484848;
-    color: #f0f0f0;
-  }
-
-  &:focus {
-    background-color: #525252;
-    color: white;
-  }
-`;
-
-const Input = styled.input`
-  display: block;
-  margin: 10px auto;
-  width: 240px;
-  padding: 8px;
-  outline: none;
-  border: 2px solid #404040;
-  border-radius: 8px;
-  background-color: #262626;
-  color: #f0f0f0;
-  font-size: 16px;
-
-  &:hover {
-    border-color: #525252;
-  }
-
-  &:focus {
-    border-color: #737373;
-  }
-`;
-
-const SearchResults = styled.ul`
-  list-style: none;
-  display: block;
-  margin: 0 auto;
-  padding: 0;
-  width: 300px;
-  height: 180px;
-  border: 2px solid #404040;
-  border-radius: 8px;
-  overflow: hidden;
-  overflow-y: scroll;
-`;
-
-const SearchResultItem = styled.button`
-  background-color: #171717;
-  color: #d4d4d4;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  font-size: 14px;
-  width: 100%;
-  border: none;
-  outline: none;
-  margin: 0;
-  padding: 4px 8px;
-  text-align: left;
-
-  &:hover {
-    background-color: #262626;
-  }
-
-  &:focus {
-    background-color: #323232;
-    color: #f0f0f0;
-  }
-`;
-
-const SearchMessage = styled.p`
-  font-size: 16px;
-  color: #525252;
-  text-align: center;
-  margin: 0;
-  padding: 4px 8px;
-  font-weight: bold;
-`;
-
 function App() {
   const [game, setGame] = useState<Game | null>(null);
   const [editMode, setEditMode] = useState(false);
   const [searchResults, setSearchResults] = useState<JSX.Element[]>([]);
   const [_, startTransition] = useTransition();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    appWindow.listen('event_clear_presence', () => {
+      setLoading(true);
+      setGame(null);
+      invoke('clear_presence').then((success) => {
+        if (!success) {
+          message('Failed to clear presence.');
+        }
+        setLoading(false);
+      });
+    });
+  }, []);
 
   function updateQuery(text: string) {
     startTransition(() => {
       setSearchResults(
         getSearchResults(text, (game: Game) => {
+          setLoading(true);
           setGame(game);
           setEditMode(false);
           invoke('update_presence', {
             game: game.title,
+          }).then((success) => {
+            if (!success) {
+              message('Failed to update presence.');
+            }
+            setLoading(false);
           });
         })
       );
@@ -151,13 +55,14 @@ function App() {
 
   return (
     <div>
-      <ImageContainer>
-        <Image src={game?.image ?? ''} />
-      </ImageContainer>
-      <NowPlaying>Now Playing</NowPlaying>
+      <div className="image-container">
+        <img src={game?.image ?? ''} />
+      </div>
+      <p className="now-playing">Now Playing</p>
       {editMode ? (
         <>
-          <Input
+          <input
+            className="search-input"
             autoFocus={true}
             autoComplete="off"
             autoCorrect="off"
@@ -167,28 +72,41 @@ function App() {
               updateQuery(event.target.value);
             }}
           />
-          <SearchResults>{searchResults}</SearchResults>
-          <Button
+          <div className="search-results">{searchResults}</div>
+          <button
+            className="regular"
             onClick={() => {
               setEditMode(false);
             }}
           >
             Cancel
-          </Button>
+          </button>
         </>
       ) : (
         <>
-          <Title>{game ? game.title : 'Nothing'}</Title>
-          <Button
+          <p className="title">{game ? game.title : 'Nothing'}</p>
+          <button
+            className="regular"
+            disabled={loading}
             onClick={() => {
               setEditMode(true);
               setSearchResults(getSearchResults(''));
             }}
           >
             Edit
-          </Button>
+          </button>
         </>
       )}
+      <RingLoader
+        color="#EB459E"
+        loading={loading}
+        cssOverride={{
+          position: 'absolute',
+          top: '16px',
+          right: '16px',
+        }}
+        size={32}
+      />
     </div>
   );
 }
@@ -198,14 +116,14 @@ function getSearchResults(query: string, select?: (game: Game) => void) {
   if (query.length === 0) {
     return [
       <li key="no-query">
-        <SearchMessage>Enter a game title above</SearchMessage>
+        <p>Enter a game title above</p>
       </li>,
     ];
   }
   if (query.length < 3) {
     return [
       <li key="too-short">
-        <SearchMessage>Keep typing...</SearchMessage>
+        <p>Keep typing...</p>
       </li>,
     ];
   }
@@ -216,33 +134,33 @@ function getSearchResults(query: string, select?: (game: Game) => void) {
       if (results.length > 100) {
         results.push(
           <li key="too-many">
-            <SearchMessage>Results capped at 100</SearchMessage>
+            <p>Results capped at 100</p>
           </li>
         );
         break;
       }
       results.push(
         <li key={i}>
-          <SearchResultItem
+          <button
             onClick={() => {
               if (select) select(game);
             }}
           >
             {game.title}
-          </SearchResultItem>
+          </button>
         </li>
       );
     }
   }
   results.push(
     <li key="manual">
-      <SearchResultItem
+      <button
         onClick={() => {
           if (select) select({ title: query });
         }}
       >
         Use '{query}'
-      </SearchResultItem>
+      </button>
     </li>
   );
   return results;
